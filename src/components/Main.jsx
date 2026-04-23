e82c2e29e00b6ded335adc6780ba92789e4bcff8
Dom4is <damir.tulemisov@yandex.ru>
2026-04-23 17:05:18 +0500

    add form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { Button } from "./ui/Button";
 import { ButtonGradient } from "./ui/ButtonGradient";
 import TestimonialsSlider from "./ui/TestimonialsSlider";
+import Test from "./ui/Test";
 import styles from "../styles/Main.module.css";
 export function Main() {
   return (
@@ -219,20 +220,74 @@ export function Main() {
         <img src="/Map_Russia.png" alt="" />
       </div>
       <section className={styles.main}>
-        <div className={styles.gradientDiv} style={{}}>
-          <h2></h2>
+        <div
+          className={styles.gradientDiv}
+          style={{
+            color: "black",
+            paddingRight: "190px",
+            paddingBottom: "90px",
+          }}
+        >
+          <h2>
+            Хотите узнать <br />
+            больше, <br /> рассчитать <br />
+            стоимость <br />
+            или забрать <br /> устройство <br />
+            на бесплатный <br /> тест?
+          </h2>
         </div>
-        <form action="" method="post" className={styles.card}>
-          <h4></h4>
-          <input type="text" className={styles.inputs} />
-          <span style={{ fontSize: "18px" }}></span>
-          <input type="number" name="" id="" className={styles.inputs} />
-          <input type="text" className={styles.inputs} />
-          <input type="checkbox" name="" id="" />
-          <span></span>
+        <form action="" method="post" className={styles.netForm}>
+          <h4>{"Услуга доступна только для юридических лиц".toUpperCase()}</h4>
+          <div>
+            <input
+              type="text"
+              name="name"
+              required
+              autocomplete="name"
+              placeholder="Ваше имя *"
+              className={styles.inputs}
+            />
+          </div>
+          <span style={{ fontSize: "18px" }}>Оставьте ваши контакты</span>
+          <div>
+            <input
+              type="tel"
+              name="tel"
+              id=""
+              placeholder="Ваш телефон +7 (___)___-__-__ *"
+              className={styles.inputs}
+              required
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="nick"
+              placeholder="Ваш ник в tg"
+              className={styles.inputs}
+              required
+            />
+          </div>
+          <div className={styles.checkboxGroup}>
+            <input
+              type="checkbox"
+              name="check"
+              id="check"
+              className={styles.check}
+              required
+            />
+            <label htmlFor="check" style={{ fontSize: "12px" }}>
+              Я даю согласие на обработку персональных данных в форме обращения
+              на странице сайта. Ознакомиться с условиями <br /> Политики
+              обработки персональных данных ООО «НЬЮКОМ ДИСТРИБЬЮШН» <br />*
+              Поля, обязательные для заполнения
+            </label>
+          </div>
+          <br />
           <ButtonGradient value={"Начать!"} />
         </form>
       </section>
+      {/* <Test /> */}
     </>
   );
 }
